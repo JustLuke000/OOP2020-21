8a6e185bdfba950a687b5ef26bdcd58b51c7ee2d
JustLuke000 <nghd.server@gmail.com>
2020-10-09 10:48:40 +0100

    First attempt

--- Labs/lab3.py
+++ Labs/lab3.py
@@ -12,17 +12,30 @@ class WordScramble:
 
     def scramble(self):
         # print what was input
-        print("The user input was: ", self.user_input)
+        print("The user input was:", self.user_input)
 
         # first scramble is just one word
+        i = 0
+        for i in range(len(self.user_input)):
+            print(self.user_input[i])
+            i = i + 1
+            if i > 1 and self.user_input[i] != ' ':
+                print(self.user_input[i])
+                print(self.user_input[(i+1)])
+                i = i + 1
+
+        print(self.user_input)
         # reverse two indices
         # particularly good to use is to switch the first two
         # and the last two
+
         # this only makes sense if you have a world that is longer than 3
 
 
         # now try to scramble one sentence
+
         # do just words first, then you can move on to work on
+
         # punctuation
 
 word_scrambler = WordScramble()

1f171bc7f38a010cff9496ca19901044174d5d19
JustLuke000 <nghd.server@gmail.con>
2020-11-27 09:56:52 +0000

    Third Attempt Working

--- Labs/Fibonacci.py
+++ Labs/Fibonacci.py
@@ -10,35 +10,34 @@ class Fibonacci:
     second_term = 1
 
     try:
-        term = int(input("Input please:"))
+        term = int(input("Please Enter a Positive Whole Integer:"))
         # enter a character, see what happens
     except ValueError as ve:
         print("A value error has happened")
-        print(ve)
+        #print(ve)
         term = 0  # keep any future usage of term from
         # crashing the program if we ran into this
     except Exception as e:
         print("I didn't realise this could happen")
-        print(e)
+        #print(e)
         term = 0
 
     print(term)
 
     if term <= 0:
-        print("Please Enter a Positive Whole Integer")
+        print("Please Enter a Positive Whole Integer:")
     elif term == 1: #The first two terms are a small exception and require special attention
         print(term," terms:")
         print(first_term)
     else:
     #Actual Fibonacci
         print("Your Input:",term)
-        print("Terms Requested ",term)
+        print("Terms Requested",term)
         counter = 0
-
         while counter < term:
+            print(first_term)
             calculation = first_term + second_term
             first_term = second_term
             second_term = calculation
-            print("\n",counter,": ",calculation)
             counter += 1
 
